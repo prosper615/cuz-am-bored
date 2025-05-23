ef781a63bdd76e06e36dd406d4dd2813b107c0da
prosper615 <prosperchiderachukwu@gmail.com>
2025-05-23 00:14:48 -0700

    ok. hope the power supply don't disappoint

--- bored.ts
+++ bored.ts
@@ -3,3 +3,11 @@
 
 
 console.log("Am very bored right now")
+
+
+
+// Here i have decided to implement a very simple version of anonymous routing
+
+// Or kinda   showing the concept of onion layer algorithm
+
+// just for play but this basic concept is what is behind stuffs like Tor ( an anonymous browser)
